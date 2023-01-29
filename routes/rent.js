2512b5c7c3ad4ef566420612be26ed4abcc17c4e
SaravanarajuR <SaravanarajuR>
2023-01-29 20:41:58 +0530

    Major Bug fixes

--- routes/rent.js
+++ routes/rent.js
@@ -7,6 +7,22 @@ function findLength() {
   const count = Home.count({});
   return count;
 }
+function filterLength(city, rate, qual, type) {
+  const count = Home.count({
+    regio1: city,
+    Rating: rate,
+    interiorQuality: qual,
+    typeOfFlat: type,
+  });
+  return count;
+}
+
+function searchLength(url) {
+  const count = Home.count({
+    $text: { $search: url },
+  });
+  return count;
+}
 
 router.get("/", async function (req, res) {
   if (req.session.isAuthenticated) {
@@ -31,10 +47,10 @@ router.get("/", async function (req, res) {
             page: req.query.page || 1,
             total: await findLength(),
             url: "",
-            filters: "",
+            filters: [],
           });
         });
-    } else {
+    } else if (req.query.search != null) {
       const page = Number(req.query.page) || 1;
       const contentPerPage = 20;
       const toSkip = (page - 1) * contentPerPage;
@@ -43,62 +59,67 @@ router.get("/", async function (req, res) {
       })
         .skip(toSkip)
         .limit(contentPerPage)
-        .then((f) => {
+        .then(async function (f) {
           f.sort((a, b) => {
             if (a.Rating >= b.Rating) return 1;
             if (a.Rating < b.Rating) return -1;
             else return 0;
           }).reverse();
-          const totalCount = f.length / 20;
           if (f) {
+            const totalCount = Math.ceil(
+              (await searchLength(req.query.search)) / 20
+            );
             res.render("rent", {
               home: f,
               page: req.query.page || 1,
               total: totalCount,
               url: req.query.search,
-              filters: "",
+              filters: [],
             });
-          } else if (req.query.filter) {
-            const page = Number(req.query.page) || 1;
-            const contentPerPage = 20;
-            const toSkip = (page - 1) * contentPerPage;
-            Home.find({
-              regio1: req.query.filter[0],
-              Rating: req.query.filter[1],
-              interiorQuality: req.query.filter[2],
-              typeOfFlat: req.query.filter[3],
-            })
-              .skip(toSkip)
-              .limit(contentPerPage)
-              .then((f) => {
-                f.sort((a, b) => {
-                  if (a.Rating >= b.totalRent) return 1;
-                  if (a.Rating < b.totalRent) return -1;
-                  else return 0;
-                }).reverse();
-                const totalCount = f.length;
-                res.render("rent", {
-                  home: f,
-                  page: req.query.page || 1,
-                  total: totalCount,
-                  url: "",
-                  filters: req.query.filter,
-                });
-              });
-          } else {
-            res.render("rent", { home: [] });
           }
         });
+    } else if (req.query.filter != null) {
+      const page = Number(req.query.page) || 1;
+      const contentPerPage = 20;
+      const toSkip = (page - 1) * contentPerPage;
+      const arr = req.query.filter.split(",");
+      Home.find({
+        regio1: arr[0],
+        Rating: arr[1],
+        interiorQuality: arr[2],
+        typeOfFlat: arr[3],
+      })
+        .skip(toSkip)
+        .limit(contentPerPage)
+        .then((f) => {
+          f.sort((a, b) => {
+            if (a.Rating >= b.totalRent) return 1;
+            if (a.Rating < b.totalRent) return -1;
+            else return 0;
+          }).reverse();
+          const totalCount = f.length;
+          res.render("rent", {
+            home: f,
+            page: req.query.page || 1,
+            total: totalCount,
+            url: "",
+            filters: req.query.filter,
+          });
+        });
+    } else {
+      res.render("rent", { home: [] });
     }
   } else {
     res.redirect("/login");
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async function (req, res) {
   const page = Number(req.query.page) || 1;
   const contentPerPage = 20;
   const toSkip = (page - 1) * contentPerPage;
+
+  //filtering
   Home.find({
     regio1: req.body.city,
     Rating: req.body.rate,
@@ -107,13 +128,18 @@ router.post("/", (req, res) => {
   })
     .skip(toSkip)
     .limit(contentPerPage)
-    .then((f) => {
+    .then(async (f) => {
       f.sort((a, b) => {
         if (a.Rating >= b.totalRent) return 1;
         if (a.Rating < b.totalRent) return -1;
         else return 0;
       }).reverse();
-      const totalCount = f.length;
+      const totalCount = await filterLength(
+        req.body.city,
+        req.body.rate,
+        req.body.quality,
+        req.body.type
+      );
       const fil = [
         req.body.city,
         req.body.rate,
