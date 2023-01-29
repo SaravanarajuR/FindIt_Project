8fb1403728470253ac734243c91c4dfa444233f8
SaravanarajuR <SaravanarajuR>
2023-01-29 14:51:28 +0530

    Filter bug clearance

--- routes/rent.js
+++ routes/rent.js
@@ -10,7 +10,7 @@ function findLength() {
 
 router.get("/", async function (req, res) {
   if (req.session.isAuthenticated) {
-    if (!req.query.search) {
+    if (!req.query.search && !req.query.filter) {
       const page = Number(req.query.page) || 1;
       const contentPerPage = 20;
       const toSkip = (page - 1) * contentPerPage;
@@ -31,6 +31,7 @@ router.get("/", async function (req, res) {
             page: req.query.page || 1,
             total: await findLength(),
             url: "",
+            filters: "",
           });
         });
     } else {
@@ -55,8 +56,35 @@ router.get("/", async function (req, res) {
               page: req.query.page || 1,
               total: totalCount,
               url: req.query.search,
-              filter: "false",
+              filters: "",
             });
+          } else if (req.query.filter) {
+            const page = Number(req.query.page) || 1;
+            const contentPerPage = 20;
+            const toSkip = (page - 1) * contentPerPage;
+            Home.find({
+              regio1: req.query.filter[0],
+              Rating: req.query.filter[1],
+              interiorQuality: req.query.filter[2],
+              typeOfFlat: req.query.filter[3],
+            })
+              .skip(toSkip)
+              .limit(contentPerPage)
+              .then((f) => {
+                f.sort((a, b) => {
+                  if (a.Rating >= b.totalRent) return 1;
+                  if (a.Rating < b.totalRent) return -1;
+                  else return 0;
+                }).reverse();
+                const totalCount = f.length;
+                res.render("rent", {
+                  home: f,
+                  page: req.query.page || 1,
+                  total: totalCount,
+                  url: "",
+                  filters: req.query.filter,
+                });
+              });
           } else {
             res.render("rent", { home: [] });
           }
@@ -86,11 +114,18 @@ router.post("/", (req, res) => {
         else return 0;
       }).reverse();
       const totalCount = f.length;
+      const fil = [
+        req.body.city,
+        req.body.rate,
+        req.body.quality,
+        req.body.type,
+      ];
       res.render("rent", {
         home: f,
         page: req.query.page || 1,
         total: totalCount,
         url: "",
+        filters: fil,
       });
     });
 });
